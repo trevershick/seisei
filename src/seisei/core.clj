b91525baccd79d796721800afe5573570a7bb032
Moosen Pairing <moosereapers@rallydev.com>
2015-03-03 08:20:02 -0500

    Add 'random' function

--- src/seisei/core.clj
+++ src/seisei/core.clj
@@ -1,60 +1,70 @@
 (ns seisei.core
-  (:require [clojure.data.json :as json])
-  (:gen-class)
+	(:require [clojure.data.json :as json])
+	(:gen-class)
 )
 
 
 
 
 (defn fix-single-ticks [jsonString]
-  (clojure.string/replace jsonString #"'" "\""))
+	(clojure.string/replace jsonString #"'" "\""))
 
 
 (defn parse [jsonString]
-  (let [fixed-string (fix-single-ticks jsonString)]
-    (json/read-str fixed-string :key-fn keyword)
-  )
+	(let [fixed-string (fix-single-ticks jsonString)]
+		(json/read-str fixed-string :key-fn keyword)
+	)
 )
 
 
 (defn tag-contents [tag]
-  (let [matcher (re-matcher #"\{\{(.+)\}\}" tag)]
-    (if (re-find matcher)
-      (get (re-groups matcher) 1) nil
-    )
-  )
+	(let [matcher (re-matcher #"\{\{(.+)\}\}" tag)]
+		(if (re-find matcher)
+			(get (re-groups matcher) 1) nil
+		)
+	)
 )
 
 (defn is-tag [clause]
-  (if (re-matches #"\{\{(.+)\}\}" clause) true false)
+	(if (re-matches #"\{\{(.+)\}\}" clause) true false)
 )
 
 
 (defn ^:private jsonify [str]
-  json/read-str (fix-single-ticks str)
+	(json/read-str (fix-single-ticks str))
 )
 
 
 (defstruct operation :name :args)
+(defn ^:private parse-operation-args [argsString]
+	(map jsonify (clojure.string/split argsString #","))
+)
 (defn parse-operation [clause]
-  (let [matcher (re-matcher #"([a-zA-Z]+)\(([^\)]+)\)" clause)]
-    (if (re-find matcher)
-      (struct-map operation
-       :name (get (re-groups matcher) 1)
-       :args (map jsonify (clojure.string/split (get (re-groups matcher) 2) #",") :key-fn keyword)
-      )
-      nil
-    )
-  )
+	(let [matcher (re-matcher #"([a-zA-Z]+)\(([^\)]+)\)" clause)]
+		(if (re-find matcher)
+			(struct-map operation
+			 :name (get (re-groups matcher) 1)
+			 :args (parse-operation-args (get (re-groups matcher) 2))
+			)
+			nil
+		)
+	)
 )
 
 
 (defmulti execute :name)
 (defmethod execute "objectId" [operation]
-  (str (java.util.UUID/randomUUID))
+	(str (java.util.UUID/randomUUID))
+)
+(defmethod execute "random" [operation]
+	( let [
+			args (:args operation)
+			argsz (count (:args operation)) ]
+		(if (= 0 (count args)) "random value here" (rand-nth args))
+	)
 )
 (defmethod execute :default [operation]
-  (str "uknown method" operation)
+	(str "uknown method" operation)
 )
 
 
@@ -62,22 +72,17 @@
 
 (defmulti process class)
 (defmethod process String [s]
-  (if (is-tag s) (execute (parse-operation (tag-contents s))) s)
+	(if (is-tag s) (execute (parse-operation (tag-contents s))) s)
 )
 (defmethod process :default [s] s)
 (defmethod process clojure.lang.PersistentArrayMap [m]
-  (into {} (for [[k v] m] [k (process v)]))
+	(into {} (for [[k v] m] [k (process v)]))
 )
 
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ("x"))
-
-
-
-
-
+	"I don't do a whole lot ... yet."
+	[& args]
+	("x"))
 
 
--- test/seisei/core_test.clj
+++ test/seisei/core_test.clj
@@ -1,16 +1,16 @@
 (ns seisei.core-test
   (:require [clojure.test :refer :all]
 			[clojure.data.json :as json]
-            [seisei.core :refer :all]))
+			[seisei.core :refer :all]))
 
 (def uuid-regex #"[a-f0-9]+-[a-f0-9]+-[a-f0-9]+-[a-f0-9]+-[a-f0-9]+")
 (def not-nil? (complement nil?))
 
 
 (def ^:private js1
-	( json/read-str 
-		( json/write-str { :id "{{objectId(3)}}" } ) 
-		:key-fn keyword 
+	( json/read-str
+		( json/write-str { :id "{{objectId(3)}}" } )
+		:key-fn keyword
 	)
 )
 
@@ -23,16 +23,16 @@
 	)
 )
 
-(def ^:private js2 
-	( json/read-str 
-		( json/write-str { :x { :y 1 :z "{{objectId(3)}}"} } ) 
-		:key-fn keyword 
+(def ^:private js2
+	( json/read-str
+		( json/write-str { :x { :y 1 :z "{{objectId(3)}}"} } )
+		:key-fn keyword
 	)
 )
 
 (deftest nested-uuid-test
 	(testing "objectId should return a uuid even when nested"
-		
+
 		(let [ processed ( process js2 ) ]
 			(is (not-nil? ( -> processed :x :z )))
 			(is (not-nil? (re-matches uuid-regex ( -> processed :x :z ))))
@@ -41,17 +41,16 @@
 )
 
 (def ^:private js3
-	( json/read-str 
-		"{ \"x\": \"{{random('blue', 'brown', 'green')}}\" }"
-		:key-fn keyword 
+	( json/read-str
+		"{ \"x\": \"{{random('blue','brown','green')}}\" }"
+		:key-fn keyword
 	)
 )
 
 (deftest random-test
 	(testing "random() should return a random value"
-		
+
 		(let [ processed ( process js3 ) ]
-			(println "processed -> " processed)
 			(is (contains? #{"blue" "brown" "green"} (:x processed)))
 		)
 	)
