0af80e969bc0e684d801717ef72891aa2eadd196
Trever Shick <trevershick@gmail.com>
2015-03-21 16:16:58 -0400

    Touch project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject seisei "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/trevershick/seisei"
   :debug true
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
