b5dbea7cfc8e5a318febabec82864a8b70a14aaf
Trever Shick <tshick@overstock.com>
2015-11-25 09:28:02 -0500

    Add doc to dispatcher.cljs

--- src/main/cljs/seisei/ui/dispatcher.cljs
+++ src/main/cljs/seisei/ui/dispatcher.cljs
@@ -1,27 +1,26 @@
 (ns seisei.ui.dispatcher
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [om.core :as om :include-macros true]
+  (:require [seisei.ui.util :refer [debug]]
     [cljs.core.async :as async]))
 
-(defonce dispatcher (async/chan 1000))
+(defonce ^:private dispatcher
+  (async/chan 1000))
 
-(def mult-c (async/mult dispatcher))
+(def ^:private mult-c
+  (async/mult dispatcher))
 
-; (def action-topic (pub dispatcher :action))
 (defn subscribe []
-   (let [the-channel (async/chan)]
+  "Takes no argument and returns a core.async channel
+  which can be polled for messages fired via seisei.ui.dispatcher/action"
+  (let [the-channel (async/chan)]
     (async/tap mult-c the-channel)
     the-channel))
 
-; (defn subscribe [topic-name]
-;   (let [the-channel (chan)]
-;     (sub action-topic topic-name the-channel)
-;     the-channel))
-
 (defn action
-  ([a]
-    (action a nil))
-  ([a data]
-    (let [message   {:action a :data data}
-          _         (println "Got Dispatcher Message - Firing " message)
-          result    (async/put! dispatcher message)])))
+  "Puts a message on the dispatcher in the form
+  {:action action-keyword :data opts}."
+  ([action-keyword]
+    (action action-keyword nil))
+  ([action-keyword opts]
+    (let [message   {:action action-keyword :data opts}
+          result    (async/put! dispatcher message)]
+      (debug "dispatcher/action put!" message))))
